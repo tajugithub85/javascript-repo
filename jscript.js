0219ba6306572c4a0f71ff99103e96ed2b1c0edd
tajugithub85 <62436203+tajugithub85@users.noreply.github.com>
2020-06-01 14:28:16 +0100

    first version js

---
+++ jscript.js
@@ -0,0 +1,11 @@
+let notecounter =0;
+function notefunction(note) {
+    var a = 1;
+    var note = prompt("please enter youe note", );
+     if (note != null) {
+
+    notecounter++; 
+     document.getElementById("note").innerHTML =
+     "you have" + "  " + "  " + notecounter + "   " + "note";
+        }
+    }
